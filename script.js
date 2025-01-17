2e37ef8c24c996368a4e5666fedac0f9a48635da
Jeff Lowe <156537356+HuntedCast@users.noreply.github.com>
2025-01-17 00:23:20 -0500

    Create basic game loop
    Human player enters an input, looping until valid. Computer randomly
    picks from 3 options. Choices are compared and a winner decided.
    Basic score tracking implemented as well.

--- script.js
+++ script.js
@@ -1,1 +1,54 @@
-console.log("Hello World!");
+// Rock, Paper, Scissors
+// 0 = Rock, 1 = Paper, 2 = Scissors
+
+function getComputerChoice() {
+    let choice = Math.floor(Math.random() * 3);
+
+    switch (choice) {
+        case 0: 
+            return "rock";
+        case 1:
+            return "paper";
+        case 2:
+            return "scissors";
+    }
+}
+
+function getHumanChoice() {
+    let message = "Rock, paper or scissors?"
+    
+    while (true) {
+        let choice = prompt(message).toLowerCase();
+
+        if (choice === "rock" || choice === "paper" || choice === "scissors") {
+            return choice;
+        }
+
+        message = "Please enter a valid input. Rock, paper or scissors?";
+    }
+}
+
+function capitalize(word) {
+    return String(word).charAt(0).toUpperCase() + String(word).slice(1);
+}
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        console.log("It's a draw!");
+        return;
+    }
+
+    if (humanChoice === "rock" && computerChoice === "scissors" || humanChoice === "paper" && computerChoice === "rock" || humanChoice === "scissors" && computerChoice === "paper") {
+        console.log(`You win! ${capitalize(humanChoice)} beats ${capitalize(computerChoice)}`);
+        humanScore++;
+    }
+    else {
+        console.log(`You lose! ${capitalize(computerChoice)} beats ${capitalize(humanChoice)}`);
+        computerScore++;
+    }
+}
+
+let computerScore = 0;
+let humanScore = 0;
+
+playRound(getHumanChoice(), getComputerChoice());
